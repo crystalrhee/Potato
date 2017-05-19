f0b8cf8c6cddc8aa0a1e6eb6e5378c56d0d95e5a
Ivan Chen <ivan.if.chen@gmail.com>
2017-05-18 20:12:01 -0400

    Initialized project.

---
+++ Potato/Potato/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Potato
+//
+//  Created by Ivan Chen on 5/18/17.
+//  Copyright © 2017 Potato Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
